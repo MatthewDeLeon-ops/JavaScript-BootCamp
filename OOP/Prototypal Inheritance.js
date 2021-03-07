584c59ef30a835acc7bf22b294f7b42bfa888518
Matthew Deleon <Matt@MacBook-Pro.local>
2021-03-06 19:54:56 -0700

    revisions made to prototypal inheritance files

--- OOP/Prototypal Inheritance.js
+++ OOP/Prototypal Inheritance.js
@@ -1,13 +1,11 @@
 // Prototypal Inheritance
 // Add a method onto instances of an object.
-// myPerson ---> Person.prototype --> Object.prototype--> null
 
-const Person = function (firstName, lastName, age, likes = [], degree_type = []) {
+const Person = function (firstName, lastName, age, likes = []) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.age = age;
   this.likes = likes;
-  this.degree_type = degree_type
 };
 
 Person.prototype.getBio = function () {
@@ -15,10 +13,6 @@ Person.prototype.getBio = function () {
 
   this.likes.forEach((like) => {
     bio += `${this.firstName} likes ${like}. `;
-  });
-  
-  this.degree_type.forEach((degree_type) => {
-    bio += `${this.degree_type} majored in ${degree_type}. `;
   });
 
   return bio;
@@ -31,36 +25,20 @@ Person.prototype.setName = function (fullName) {
   this.lastName = names[1];
 };
 
-// Example of a new class that can have the same properties as another class using extends. [Subclass]
-// Override functionity and features in the {}
-class Otherperson extends Person {
-  constructor(firstName, lastName, age, likes,degree_type) {
-      super(firstName,lastName,age,likes)
-      this.degree_type = degree_type
-    }
-  getBio() {
-    return `${this.firstName} ${this.lastName} majored in ${this.degree_type}`
-    // Matthew majored in Business Administration
-  }
-  getGraduationYear() {
-    return 2019
-  }
-}
+const me = new Person("Matthew", "De Leon", 23, ["Javascript", "Family Time"]);
 
-const me = new Otherperson ("Matthew", "De Leon", 23, ["Javascript", "Family Time"], "Business Administration");
+me.getBio = function () {
+  return "This is cool";
+};
 
-// me.getBio = function () {
-//   return "This is cool";
-// };
-
-// uncomment to view the output of subclass method Other person.
 console.log(me)
-console.log(me.getGraduationYear())
-
-// uncomment to view another method of getting the output customized with just the components listed.
-// me.setName("Matthew DeLeon");
+// me.setName("Matthew De Leon");
 // console.log(me.getBio());
 
 const person2 = new Person("Jacqueline", "Cai", 30);
+
+// Person.prototype.getBio = function() {
+//     return 'Testing Testing'
+// }
 
 console.log(person2.getBio());
--- OOP/class syntax/prototypal inheritance with class syntax.js
+++ OOP/class syntax/prototypal inheritance with class syntax.js
@@ -1,39 +1,66 @@
 // Prototypal Inheritance
 // Add a method onto instances of an object.
+// myPerson ---> Person.prototype --> Object.prototype--> null
 
-class Person {
-  constructor(firstName, lastName, age, likes = []) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.likes = likes;
-  }
+const Person = function (firstName, lastName, age, likes = [], degree_type = []) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.likes = likes;
+  this.degree_type = degree_type
+};
+
+Person.prototype.getBio = function () {
+  let bio = `${this.firstName} is ${this.age}.`;
+
+  this.likes.forEach((like) => {
+    bio += `${this.firstName} likes ${like}. `;
+  });
+  
+  this.degree_type.forEach((degree_type) => {
+    bio += `${this.degree_type} majored in ${degree_type}. `;
+  });
+
+  return bio;
+  // Placing a method (i.e. getBio) inside a property (i.e.prototype) within a constructor (i.e. Person) will grant access to individual components for all instances
+};
+
+Person.prototype.setName = function (fullName) {
+  const names = fullName.split(" ");
+  this.firstName = names[0];
+  this.lastName = names[1];
+};
+
+// Example of a new class that can have the same properties as another class using extends. [Subclass]
+// Override functionity and features in the {}
+class Otherperson extends Person {
+  constructor(firstName, lastName, age, likes,degree_type) {
+      super(firstName,lastName,age,likes)
+      this.degree_type = degree_type
+    }
   getBio() {
-    let bio = `${this.firstName} is ${this.age}.`;
-    this.likes.forEach((like) => {
-      bio += `${this.firstName} likes ${like}. `;
-    });
-    return bio;
+    return `${this.firstName} ${this.lastName} majored in ${this.degree_type}`
+    // Matthew majored in Business Administration
   }
-  setName(fullName) {
-    const names = fullName.split(" ");
-    this.firstName = names[0];
-    this.lastName = names[1];
+  getGraduationYear() {
+    return 2019
   }
 }
 
-const myPerson = new Person("Matthew", "DeLeon", 23, ["Javascript"]);
-console.log(myPerson.getBio());
+const me = new Otherperson ("Matthew", "De Leon", 23, ["Javascript", "Family Time"], "Business Administration");
+
+// me.getBio = function () {
+//   return "This is cool";
+// };
+
+// uncomment to view the output of subclass method Other person.
+console.log(me)
+console.log(me.getGraduationYear())
+
+// uncomment to view another method of getting the output customized with just the components listed.
+// me.setName("Matthew DeLeon");
+// console.log(me.getBio());
 
 const person2 = new Person("Jacqueline", "Cai", 30);
 
-// Person.prototype.getBio = function() {
-//     return 'Testing Testing'
-// }
-
 console.log(person2.getBio());
-
-// challenge
-// convert the hangman application with constructors and 4 methods used.
-// you should not need to change app.js to make things work
-// test your work. DONT CLICK PLAY TILL DONE
