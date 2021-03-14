b4833b54c2f9244720db4295718a238ee1bee126
Matthew De Leon <mattjdeleon@gmail.com>
2021-03-14 00:09:04 -0700

    requests lesson 105

--- Asyncronous :) /appv1.js
+++ Asyncronous :) /appv1.js
@@ -4,38 +4,41 @@ const componentsEL = document.querySelector("#components");
 const guessEL = document.querySelector("#guess");
 const Word1 = new Guesstheword("dog", 2);
 
-window.addEventListener("keypress", function (e) {
+window.addEventListener("keypress", (e) => {
   const guess = String.fromCharCode(e.charcode);
   Word1.makeanattempt(guess);
   componentsEL.textContext = Word1.challenge();
   guessEL.textContext = Word1.guessingstatus();
   console.log(Word1.status);
 });
-// componentsEL.textContent = Word1.challenge();
+componentsEL.textContent = Word1.challenge();
 
 // Making an http request process using a constructor function provided by my browser
 const request = new XMLHttpRequest();
 
-request.addEventListener("readystatechange",(e) => {
-    if (e.target.readyState === 4) {
-      console.log(e.target);
-    }
-  })
-  request.open("GET", "http://puzzle.mead.io/puzzle")
-  request.send()
+request.addEventListener("readystatechange", (e) => {
+  if (e.target.readyState === 4 && e.target.status === 200) {
+    const data = JSON.parse(e.target.responseText);
+    console.log(data);
+  } else if (e.target.readyState == 4 && e.target.status == 400) {
+    console.log("A 400 Error has occured");
+  }
+});
+request.open("GET", "http://puzzle.mead.io/puzzle?wordCount=3");
+request.send();
 
 // XMLHttpRequest.open(method, url[, async[, user[, password]]])
 // request.open: Sets the request method, request URL, and synchronous flag.
 // request.send Initiates the request.
 
-
-
 /// examples and notes
 // HTTP (HyperText Transfer Protocol)
 // request = what the developer wants the function to do
-// response = What was done in response to the request.
+// response = What was done in response to the request. -- see responsetext
 // when you enter a page by pasting a url. A url request is initiated.
 // to view a request and page info access the web tools by clicking F12 in chrome. View the network section.
+// httpstatuses.com look at
+// http messages mozilla look at
 
 ////examples of get requests
 // when you type a weblink in you are creating a get request (get http method)
